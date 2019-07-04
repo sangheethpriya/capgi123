7e7632a889736ecf555bf2912b39b538955c0e41
saannama <saannama@DIN78000371.corp.capgemini.com>
2019-07-04 11:57:08 +0530

    my first commit

---
+++ sample/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,13 @@
+package org.cap.demo;
+
+public class MainClass {
+	public static void main(String args[]) {
+	System.out.println("welcome sales application");
+	SalesClass obj=new SalesClass();
+	obj.show();
+	display();
+	}
+		public static void display()
+		{
+			System.out.println("hello");
+}}
---
+++ sample/src/main/java/org/cap/demo/SalesClass.java
@@ -0,0 +1,11 @@
+package org.cap.demo;
+
+public class SalesClass {
+	private int[] arr= {2,3,4,5};
+	public void show() {
+		for(int num:arr) {
+			System.out.println(num);
+		}
+	}
+
+}
